b2c3d2246f13e7bd5548d9205b479b76be757c1d
Alexander Rodionov <alx1dr@gmail.com>
2019-07-28 18:07:55 +0300

    reduce intermediate steps using itertools
    maybe more cryptic code

--- 2ch.py
+++ 2ch.py
@@ -9,72 +9,70 @@ export FLASK_APP=2ch.py; python3 -m flask run --host=0.0.0.0 &
 from flask import Flask
 import requests
 import string
+import itertools
 
 boards = ['news', 'po', 'b']
 
-def get_threads(board): return requests.get(f'https://2ch.hk/{board}/threads.json').json()['threads']
+def get_threads(board):
+    board_threads = requests.get(f'https://2ch.hk/{board}/threads.json').json()['threads']
+    for thread in board_threads:
+        thread['board'] = board # add board label
+    return board_threads
 
-template = string.Template(
-'''
-<table>
-$threads
-</table>
-
-<style>
-table {
-    margin: 0px 150px 0px 150px;
-    background-color: rgba(0, 0, 0, 0.03);
-}
-
-th {
-    font-family: Verdana;
-    font-size: 9pt;
-    font-weight: normal;
-    text-align: left;
-}
-
-a { color: black; text-decoration: none; }
-a:visited { color: rgb(200,200,200); }
-a:hover { text-decoration: underline; }
-</style>
-''')
+def thread2html(thread):
+    subject      = thread['subject'][:70]
+    views        = thread['views']
+    score        = round(thread['score'], 1)
+    comments     = thread['posts_count']
+    board        = thread['board']
+    url          = f"https://2ch.hk/{board}/res/{thread['num']}.html"
+    return f'''
+    <tr>
+        <th><a href='{url}'>{subject}</a></th>
+        <th>score {score}</th>
+        <th>views {views}</th>
+        <th>comments {comments}</th>
+        <th>{board}</th>
+    </tr>
+    '''
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    threads = []
-    for board in boards:
-        board_threads = get_threads(board)
+    return string.Template(
+    '''
+    <table>
+    $threads
+    </table>
 
-        for bt in board_threads:
-            bt['board'] = board
+    <style>
+    table {
+        margin: 0px 150px 0px 150px;
+        background-color: rgba(0, 0, 0, 0.03);
+    }
 
-        threads += board_threads
+    th {
+        font-family: Verdana;
+        font-size: 9pt;
+        font-weight: normal;
+        text-align: left;
+    }
 
-    threads_str = ''
-    # for thread in sorted(threads, key=lambda x: x['views'], reverse=True):
-    for thread in sorted(threads, key=lambda x: x['score'], reverse=True):
-        subject      = thread['subject'][:70]
-        views        = thread['views']
-        score        = round(thread['score'], 1)
-        comments     = thread['posts_count']
-        board        = thread['board']
-        url          = f"https://2ch.hk/{board}/res/{thread['num']}.html"
-        threads_str += f'''
-            <tr>
-            <th><a href='{url}'>{subject}</a></th>
-            <th>score {score}</th>
-            <th>views {views}</th>
-            <th>comments {comments}</th>
-            <th>{board}</th>
-            </tr>
-        '''
-
-    html_str = template.substitute(threads=threads_str)
-
-    # html = pathlib.Path(__file__).parent /'2ch.html'
-    # html.write_text(html_str)
-    # webbrowser.open(html.resolve().as_uri())
-
-    return html_str
+    a { color: black; text-decoration: none; }
+    a:visited { color: rgb(200,200,200); }
+    a:hover { text-decoration: underline; }
+    </style>
+    '''
+    ).substitute(
+        threads = ''.join(
+            thread for thread in map(
+                thread2html, 
+                sorted(
+                    itertools.chain.from_iterable(map(get_threads, boards)), 
+                    key=lambda thread: thread['score'], 
+                    reverse=True,
+                )
+            )
+        )
+    )
