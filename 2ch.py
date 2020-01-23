dca697bc414460bc9fc04075527cba5628afc896
Alexander Rodionov <alx1dr@gmail.com>
2020-01-24 00:25:12 +0300

    better ranking

--- 2ch.py
+++ 2ch.py
@@ -1,4 +1,5 @@
 '''
+TODO: githooks (git push github, git push prod) / or git hooks on github
 sudo apt update
 sudo apt -y upgrade
 sudo apt install -y python3-pip
@@ -11,8 +12,32 @@ import requests
 import string
 import itertools
 import operator
+import datetime
+
 
 boards = ['news', 'po', 'b']
+
+
+def ago(e):
+    t = round(e / 1000)
+    n = round(t /   60)
+    r = round(n /   60)
+    o = round(r /   24)
+    i = round(o /   30)
+    a = round(i /   12)
+    if   e <  0: return              'just now'
+    elif t < 10: return              'just now'
+    elif t < 45: return str(t) + ' seconds ago'
+    elif t < 90: return          'a minute ago'
+    elif n < 45: return str(n) + ' minutes ago'
+    elif n < 90: return           'an hour ago' 
+    elif r < 24: return str(r) +   ' hours ago'
+    elif r < 36: return             'a day ago'
+    elif o < 30: return str(o) +    ' days ago'
+    elif o < 45: return           'a month ago'
+    elif i < 12: return str(i) +  ' months ago'
+    elif i < 18: return            'a year ago'
+    else:        return str(a) +   ' years ago'
 
 def get_threads(board):
     board_threads = requests.get(f'https://2ch.hk/{board}/threads.json').json()['threads']
@@ -21,21 +46,30 @@ def get_threads(board):
     return board_threads
 
 def thread2html(thread):
-    subject      = thread['subject'][:70]
-    views        = thread['views']
-    score        = round(thread['score'], 1)
-    comments     = thread['posts_count']
-    board        = thread['board']
-    url          = f"https://2ch.hk/{board}/res/{thread['num']}.html"
+    subject     = thread['subject'][:70]
+    time_ago    = thread['time_ago']
+    posts_count = thread['posts_count']
+    board       = thread['board']
+    url         = f"https://2ch.hk/{board}/res/{thread['num']}.html"
     return f'''
     <tr>
         <th><a href='{url}'>{subject}</a></th>
-        <th>score {score}</th>
-        <th>views {views}</th>
-        <th>comments {comments}</th>
+        <th>{time_ago}</th>
+        <th>posts {posts_count}</th>
         <th>{board}</th>
     </tr>
     '''
+
+def add_ago_to_last_day_threads(threads):
+    out = []
+    for thread in threads:
+        now = datetime.datetime.now() 
+        ts = datetime.datetime.fromtimestamp(thread['timestamp']) # also try lasthit
+        if now - ts > datetime.timedelta(days=1):
+            continue
+        thread['time_ago'] = ago(now.timestamp() - ts.timestamp())
+        out.append(thread)
+    return out
 
 app = Flask(__name__)
 
@@ -43,7 +77,8 @@ app = Flask(__name__)
 def hello_world():
     _ = map(get_threads, boards)
     _ = itertools.chain.from_iterable(_)
-    _ = sorted(_, key = operator.itemgetter('score'), reverse = True,)
+    _ = add_ago_to_last_day_threads(_)
+    _ = sorted(_, key = operator.itemgetter('posts_count'), reverse = True,)
     _ = map(thread2html, _)
     _ = ''.join(thread for thread in _)
     _ = string.Template(
