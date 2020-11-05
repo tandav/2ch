6d8fa69a78c6e15f5eb93082f7bcf0b18da12496
Alexander Rodionov <alx1dr@gmail.com>
2020-11-06 02:43:16 +0300

    upd docs

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 '''
 https://2ch.hk/api
+https://github.com/4chan/4chan-API
 TODO: githooks (git push github, git push prod) / or git hooks on github
+
 sudo apt update
 sudo apt -y upgrade
 sudo apt install -y python3-pip
 python3 -m pip install Flask
-curl -O https://raw.githubusercontent.com/tandav/2ch/master/2ch.py
-export FLASK_APP=2ch.py; python3 -m flask run --host=0.0.0.0 &
+sudo apt install git-all 
+git clone https://github.com/tandav/2ch.git
+cd 2ch
+python3 server.py
 '''
 
 from flask import Flask
@@ -20,7 +24,7 @@ app = Flask(__name__, static_folder='', static_url_path='')
 
 @app.route('/')
 def root():
-    return '''\
+    return '''
     <a href='/2ch'>2ch<a>
     <br>
     <a href='/4ch'>4ch<a>
