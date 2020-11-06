6e4a4bd68e69a22a583a2b6daf6573b575a7b4c6
Alexander Rodionov <alx1dr@gmail.com>
2020-11-06 21:15:53 +0300

    upd cli

--- cli.py
+++ cli.py
@@ -16,6 +16,10 @@ class color:
     UNDERLINE = lambda s: '\033[4m'  + str(s) + '\033[0m'
 
 
+def color_i(s, i):
+    return f"\x1b[38;5;{i}m{s}\033[0m"
+
+
 while True:
     threads = itertools.chain(
         util.boards_threads(_4ch.board_threads, _4ch.boards),
@@ -36,14 +40,43 @@ while True:
         print(
             color.YELLOW(f'{i}..{len(threads)}'),
             color.BLUE(thread['board']),
-            f"{posts_count:>4}",
+            color.WHITE(f"{posts_count:>4}"),
             thread['time_ago'],
             color.CYAN(f'{sleep_time} sleep'),
             thread['url'],
         )
-        title_color = color.GREEN if posts_count >= 100 else color.WHITE
-        print(title_color(title))
+        # title_color = color.GREEN if posts_count >= 100 else color.WHITE
+        if   posts_count <   5: title_color = 236
+        elif posts_count <  10: title_color = 237
+        elif posts_count <  15: title_color = 238
+        elif posts_count <  20: title_color = 239
+        elif posts_count <  25: title_color = 240
+        elif posts_count <  30: title_color = 241
+        elif posts_count <  35: title_color = 242
+        elif posts_count <  40: title_color = 243
+        elif posts_count <  45: title_color = 244
+        elif posts_count <  50: title_color = 245
+        elif posts_count <  55: title_color = 246
+        elif posts_count <  60: title_color = 247
+        elif posts_count <  65: title_color = 248
+        elif posts_count <  70: title_color = 249
+        elif posts_count <  75: title_color = 250
+        elif posts_count <  80: title_color = 251
+        elif posts_count <  85: title_color = 252
+        elif posts_count <  90: title_color = 253
+        elif posts_count <  95: title_color = 254
+        elif posts_count < 100: title_color = 255
+        elif posts_count < 300: title_color = 82  # green
+        elif posts_count < 500: title_color = 226 # yellow
+        elif posts_count>= 500: title_color = 196 # red
+        print(color_i(title, title_color))
         # print(color.GREEN('=' * 72), color.WHITE(thread['time_ago']))
-        # time.sleep(0.7)
         print('─'*100)
         time.sleep(sleep_time)
+
+# s = 7777777777777777777777
+# for i in range(87, 81, -1):
+# for i in range(256):
+#     print(color_i(i, i))
+    # print(f"\x1b[38;5;{i}m {i}")
+    # print(f'\033[87m{s}\033[0m')
